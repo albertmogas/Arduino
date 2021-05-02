b9a9a7797ab62d6bb7de66027546ed43801bb607
albertmogas <albert.mogas.2010@lacetania.cat>
2021-05-02 18:53:47 +0200

    Update Ex3__ex1_ex2_.ino

--- Fade LEDs/Ex3__ex1_ex2_/Ex3__ex1_ex2_.ino
+++ Fade LEDs/Ex3__ex1_ex2_/Ex3__ex1_ex2_.ino
@@ -14,7 +14,7 @@ const int led2 = 6;          // donar nom al pin 6 de l’Arduino
 const int led3 = 9;          // donar nom al pin 9 de l’Arduino
 const int led4 = 10;         // donar nom al pin 10 de l’Arduino
 const int led5 = 11;         // donar nom al pin 11 de l’Arduino
-const int button = 2;        // donar nom al pin 2 de l'Arduino
+const int buttonPin = 2;        // donar nom al pin 2 de l'Arduino
 int velocitat = 500;         // velocitat de l'acció en ms
 
 //********** Setup ****************************************************************
@@ -26,7 +26,7 @@ pinMode(led0, OUTPUT);     // definir el pin 3 com una sortida
   pinMode(led3, OUTPUT);     // definir el pin 9 com una sortida
   pinMode(led4, OUTPUT);     // definir el pin 10 com una sortida
   pinMode(led5, OUTPUT);     // definir el pin 11 com una sortida
-  pinMode(button, INPUT);    // definir el pin 2 com una entrada
+  pinMode(buttonPin, INPUT);    // definir el pin 2 com una entrada
 }
 
 
